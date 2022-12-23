820500fa130509da3bc98e683e227d7be711579a
imaheshaher <maheshaher035@gmail.com>
2022-12-23 09:14:31 +0530

    added user api

--- src/reduxpractice/index.js
+++ src/reduxpractice/index.js
@@ -2,6 +2,9 @@
 import {createStore} from 'redux'
 import Counter from './Counter'
 import {Provider} from 'react-redux'
+import axios from 'axios'
+import { useState } from 'react'
+
 const intialState = {
     count : 0
 }
@@ -31,6 +34,20 @@ store.dispatch({type:"INCREMENT"})
 store.dispatch({type:"DECREMENT"})
 store.dispatch({type:"DECREMENT"})
 const IndexApp= () => {
+    const loadData = async() => {
+
+        const postData = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        console.log(postData.data,"postdata")
+    }
+
+    const loadProduct = async() => {
+        const products =  await axios.get('http://65.1.208.185:8000/product/get')
+        console.log(products,"products")
+    }
+    useState(() => {
+        loadData();
+        loadProduct()
+    },[])
     return(
 
         <Provider store ={store}>
